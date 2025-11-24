03d927d1c92e8e9bc3b8fd8a678ebc0a1e51b9d9
hismyhill <owolabismaeel@gmail.com>
2025-11-24 14:59:06 +0100

    chore: Initial project setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,1 +1,9 @@
-rnf;
+import { View, Text } from "react-native";
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+}
---
+++ src/components/ScreenWrapper.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+interface ScreenWrapperProps {
+  children: any;
+  bg: string;
+}
+
+function ScreenWrapper(children, bg): ScreenWrapperProps {
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 5 : 30;
+
+  return (
+    <View style={{ paddingTop, backgroundColor: bg, flex: 1 }}>{children}</View>
+  );
+}
+
+export default ScreenWrapper;
